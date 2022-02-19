5e81fcb0496702336c0d245b493e88ab91273751
nathanieladiah <nathanieladiah@gmail.com>
2022-02-19 14:36:58 -0400

    Change divider based on window size

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@
 import React from "react";
 
 import './scss/styles.css';
+import useWindowDimensions from "./windowDimensions";
 import data from "./data.js"
 
 import mobileDivider from "./images/pattern-divider-mobile.svg"
+import desktopDivider from "./images/pattern-divider-desktop.svg"
 import die from "./images/icon-dice.svg"
 
 function App() {
   const [adviceData, setAdviceData] = React.useState(data)
-
   const [count, setCount] = React.useState(0)
+
+  const {height, width } = useWindowDimensions();
 
   React.useEffect(() => {
     fetch(	"https://api.adviceslip.com/advice")
@@ -36,8 +39,8 @@ function App() {
     <div className='advice'>
       <h2 className='advice__heading'>ADVICE #{adviceData['slip']['id']}</h2>
       <p className='advice__content'>{adviceData['slip']['advice']}</p>
-      <img className='advice__divider' src={mobileDivider}/>
-      <div className='advice__dice' onClick={getNewAdvice}><img src={die}></img></div>
+      <img className='advice__divider' src={width < 640 ? mobileDivider : desktopDivider} alt=""/>
+      <div className='advice__dice' onClick={getNewAdvice}><img src={die} alt=""></img></div>
     </div>
   );
 }
